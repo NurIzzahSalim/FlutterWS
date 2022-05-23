4cfe9c034a98b4204f130be6e45314ea9c09374c
NUR IZZAH <n.izasalim1803@gmail.com>
2022-05-23 22:03:28 +0800

    23.5.2022

--- mytutor_admin/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-class CONSTANTS {
-  static String server = "http://10.31.105.58";   
-}// TODO Implement this library.
--- mytutor_lab2/android/app/src/main/kotlin/com/example/mytutor_lab2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mytutor_lab2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
